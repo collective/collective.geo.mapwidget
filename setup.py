7714d6fbc46b864c24cf6c116ada0628b2ab6587
Giorgio Borelli <casaborelli@gmail.com>
2012-03-25 16:31:36 +0200

    added gitignore and readme.rst

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.6.dev0'
 setup(name='collective.geo.mapwidget',
       version=version,
       description="collective.geo mapwidget",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       classifiers=["Framework :: Plone",
         "Topic :: Internet",
