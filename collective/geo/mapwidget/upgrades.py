770bf4998c3b12bd2b21f1ead8556f855dd9d690
Giorgio Borelli <casaborelli@gmail.com>
2013-05-30 18:34:45 +0200

    added colorpicker to upgrade step

--- collective/geo/mapwidget/upgrades.py
+++ collective/geo/mapwidget/upgrades.py
@@ -11,8 +11,11 @@ def upgrade_to_17(context):
     context.runImportStepFromProfile(default_profile, 'jsregistry')
     context.runImportStepFromProfile(default_profile, 'controlpanel')
 
-    # install collective.z3cform.mapwidget
+    # install collective.z3cform.mapwidget and colorpicker
     qi = getToolByName(context, 'portal_quickinstaller')
+
+    if not qi.isProductInstalled('collective.z3cform.colorpicker'):
+        qi.installProduct('collective.z3cform.colorpicker')
 
     if not qi.isProductInstalled('collective.z3cform.mapwidget'):
         qi.installProduct('collective.z3cform.mapwidget')
